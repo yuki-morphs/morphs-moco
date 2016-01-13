5bcf6dbd4b3b86e75ef10e238c573df3ad9c28f7
Yukio Matsui <yuki.morphs@gmail.com>
2016-01-13 22:26:32 +0900

    Changed assets pulish dir path from build to build/assets to be consistent with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build/assets'),
+    publicPath: "/assets/",
     filename: 'bundle.js'
   },
   module: {
