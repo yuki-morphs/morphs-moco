5cfd93220ba096b5439093a45e78c39df4e31489
Yukio Matsui <yuki.morphs@gmail.com>
2016-01-13 22:31:59 +0900

    Renamed index.js to main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: path.resolve(__dirname, 'src/main.js'),
   output: {
     path: path.resolve(__dirname, 'build/assets'),
     publicPath: "/assets/",
